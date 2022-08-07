acf2e54bc7f3ef0f1cf4a3dbf6fe915bcf37fc98
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-07 22:56:41 +0800

    update

--- _next/static/chunks/pages/our-menu-ad4f5871438e05d6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[505],{5917:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/our-menu",function(){return c(9242)}])},9242:function(a,b,c){"use strict";c.r(b);var d=c(5893);c(7294);var e=function(){return(0,d.jsx)("div",{})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=5917)}),_N_E=a.O()}])
--- _next/static/zTYPO5e1OrrcfD4Xed5Yp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-62b066cfedb66898.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/our-menu":["static/chunks/pages/our-menu-ad4f5871438e05d6.js"],sortedPages:["/","/_app","/_error","/our-menu"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
