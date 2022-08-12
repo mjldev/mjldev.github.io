42134c7dcb78ed8e6b7a6c51686d99a9b6d426f4
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-12 16:06:36 +0800

    update

--- _next/static/NZ2sN-dz9w6R1h00u1JRy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6449a014be2d3ac0.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
