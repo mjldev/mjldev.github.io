d9ba894994fefb9673b64836381cd3d64bb98dc0
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-19 15:02:15 +0800

    update

--- components/Testimonial/TestimonialItem.jsx
+++ components/Testimonial/TestimonialItem.jsx
@@ -1,22 +1,24 @@
+import React from 'react'
+
 export const TestimonialItem = [
-    {
-      name: "Review by Lorem Ipsum",
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
-    },
-    {
-      name: "Review by Lorem Ipsum",
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
-    },
-    {
-      name: "Review by Lorem Ipsum",
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
-    },
-    {
-      name: "Review by Lorem Ipsum",
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
-    },
-    {
-      name: "Review by Lorem Ipsum",
-      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
-    },
-  ];
+  {
+    name: "Review by Lorem Ipsum",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
+  },
+  {
+    name: "Review by Lorem Ipsum",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
+  },
+  {
+    name: "Review by Lorem Ipsum",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
+  },
+  {
+    name: "Review by Lorem Ipsum",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
+  },
+  {
+    name: "Review by Lorem Ipsum",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias obcaecati aperiam itaque et porro. Odit dolor placeat praesentium! Asperiores.",
+  },
+];
