3145bd2c44ab620fddfa285cab61f147d4d3931d
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-27 02:01:58 +0800

    update

--- pages/our-menu.jsx
+++ pages/our-menu.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import Overlay from "../components/Header/Overlay";
 import Title from "../components/Header/Title";
 import pork_bg from "../assets/images/pork_bg.jpg";
 import beef_bg from "../assets/images/beef_bg.jpg";
 import chicken_bg from "../assets/images/chicken_bg.jpg";
 import NewsletterForm from "../components/Newsletter/NewsletterForm";
 import ButtonPrimary from "../components/Button/ButtonPrimary";
-import BackgroundImage from "../components/Header/BackgroundImage";
 import ChickenMenu from "../components/MenuTab/ChickenMenu";
 import PorkMenu from "../components/MenuTab/PorkMenu";
 import BeefMenu from "../components/MenuTab/BeefMenu";
@@ -18,15 +16,29 @@ const OurMenu = () => {
 
   return (
     <div className="overflow-hidden">
-      <header className="-mt-20">
+      <header className="tablet:-mt-20">
         {chickenTab && (
-          <BackgroundImage url={chicken_bg.src} alt="Chicken Adobo" />
+          <img
+            src={chicken_bg.src}
+            alt="Chicken"
+            className="absolute w-full h-full object-cover"
+          />
         )}
-        {porkTab && <BackgroundImage url={pork_bg.src} alt="Pork Pata Humba" />}
+        {porkTab && (
+          <img
+            src={pork_bg.src}
+            alt="Pork"
+            className="absolute w-full h-full object-cover"
+          />
+        )}
         {beefTab && (
-          <BackgroundImage url={beef_bg.src} alt="Special Beef Soup" />
+          <img
+            src={beef_bg.src}
+            alt="Pork"
+            className="absolute w-full h-full object-cover"
+          />
         )}
-        <Overlay />
+        <div className="absolute bg-black opacity-50 w-full h-full" />
         <div className="container relative flex justify-center flex-col min-h-[100vh] max-w-full mr-auto ml-auto">
           <div className="grid grid-col-1 justify-items-center text-center gap-8 px-4">
             <Title heading="MJL KITCHEN MENU" />
@@ -36,45 +48,43 @@ const OurMenu = () => {
       </header>
       <main>
         <section>
-          <div className="container relative max-w-full">
-            <div className="flex">
-              <button
-                onClick={() => {
-                  setChickenTab(true), setPorkTab(false), setBeefTab(false);
-                }}
-                className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
-                  chickenTab
-                    ? "bg-white text-personal-textPrimary"
-                    : "bg-transparent text-white"
-                }`}
-              >
-                chicken
-              </button>
-              <button
-                onClick={() => {
-                  setChickenTab(false), setPorkTab(true), setBeefTab(false);
-                }}
-                className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
-                  porkTab
-                    ? "bg-white text-personal-textPrimary"
-                    : "bg-transparent text-white"
-                }`}
-              >
-                pork
-              </button>
-              <button
-                onClick={() => {
-                  setChickenTab(false), setPorkTab(false), setBeefTab(true);
-                }}
-                className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
-                  beefTab
-                    ? "bg-white text-personal-textPrimary"
-                    : "bg-transparent text-white"
-                }`}
-              >
-                beef
-              </button>
-            </div>
+          <div className="container flex max-w-full relative">
+            <button
+              onClick={() => {
+                setChickenTab(true), setPorkTab(false), setBeefTab(false);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                chickenTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              chicken
+            </button>
+            <button
+              onClick={() => {
+                setChickenTab(false), setPorkTab(true), setBeefTab(false);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                porkTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              pork
+            </button>
+            <button
+              onClick={() => {
+                setChickenTab(false), setPorkTab(false), setBeefTab(true);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                beefTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              beef
+            </button>
           </div>
         </section>
         <section>
