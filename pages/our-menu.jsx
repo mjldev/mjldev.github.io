c325cef0bc57928435e2101479799e5cb265cf72
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-27 10:43:54 +0800

    update

--- pages/our-menu.jsx
+++ pages/our-menu.jsx
@@ -1,67 +1,88 @@
 import React, { useState } from "react";
+import Overlay from "../components/Header/Overlay";
+import Title from "../components/Header/Title";
+import pork_bg from "../assets/images/pork_bg.jpg";
+import beef_bg from "../assets/images/beef_bg.jpg";
+import chicken_bg from "../assets/images/chicken_bg.jpg";
 import NewsletterForm from "../components/Newsletter/NewsletterForm";
-import { MenuTab } from "../components/MenuTab/MenuTab";
+import ButtonPrimary from "../components/Button/ButtonPrimary";
+import BackgroundImage from "../components/Header/BackgroundImage";
+import ChickenMenu from "../components/MenuTab/ChickenMenu";
+import PorkMenu from "../components/MenuTab/PorkMenu";
+import BeefMenu from "../components/MenuTab/BeefMenu";
 
 const OurMenu = () => {
-  const [currentTab, setCurrentTab] = useState("1");
-
-  const handleTabClick = (e) => {
-    setCurrentTab(e.target.id);
-  };
+  const [chickenTab, setChickenTab] = useState(true);
+  const [porkTab, setPorkTab] = useState(false);
+  const [beefTab, setBeefTab] = useState(false);
 
   return (
     <div className="overflow-hidden">
       <header>
-        {MenuTab.map((MenuTab, i) => (
-          <div key={i} className="w-screen object-contain">
-            {currentTab === `${MenuTab.id}` && (
-              <div className="relative">
-                <img
-                  src={MenuTab.image}
-                  alt={MenuTab.alt}
-                  className="absolute w-full h-full object-cover"
-                />
-                <div className="absolute bg-black opacity-50 w-full h-full" />
-                <div className="container flex justify-center flex-col min-h-[60vh] max-w-[90%] relative">
-                  <div className="flex flex-col items-start gap-y-4">
-                    <p className="font-oswald font-bold text-2xl desktop:text-4xl text-white uppercase underline underline-offset-8  decoration-4">
-                      must try!
-                    </p>
-                  </div>
-                </div>
+        <div className="w-screen object-contain">
+          <div className="relative">
+            <img
+              src={chicken_bg.src}
+              alt="Chicken"
+              className="absolute w-full h-full object-cover"
+            />
+            <div className="absolute bg-black opacity-50 w-full h-full" />
+            <div className="container flex justify-center flex-col min-h-[60vh] max-w-[90%] relative">
+              <div className="flex flex-col items-start gap-y-4">
+                <p className="font-oswald font-bold text-2xl desktop:text-4xl text-white uppercase underline underline-offset-8  decoration-4">
+                  must try!
+                </p>
               </div>
-            )}
+            </div>
           </div>
-        ))}
+        </div>
       </header>
       <main>
-        <section>
-          <div className="container flex justify-between max-w-full relative">
-            {MenuTab.map((MenuTab, i) => (
-              <button
-                className="flex-1 bg-transparent hover:bg-gray-300 disabled:bg-gray-500 border transition-all duration-300 ease-ou py-4"
-                key={i}
-                id={MenuTab.id}
-                disabled={currentTab === `${MenuTab.id}`}
-                onClick={handleTabClick}
-              >
-                {MenuTab.tabTitle}
-              </button>
-            ))}
+      <section>
+          <div className="container flex max-w-full relative">
+            <button
+              onClick={() => {
+                setChickenTab(true), setPorkTab(false), setBeefTab(false);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                chickenTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              chicken
+            </button>
+            <button
+              onClick={() => {
+                setChickenTab(false), setPorkTab(true), setBeefTab(false);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                porkTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              pork
+            </button>
+            <button
+              onClick={() => {
+                setChickenTab(false), setPorkTab(false), setBeefTab(true);
+              }}
+              className={`flex-1 font-oswald font-semibold text-4xl uppercase py-10 border-t ${
+                beefTab
+                  ? "bg-white text-personal-textPrimary"
+                  : "bg-transparent text-white"
+              }`}
+            >
+              beef
+            </button>
           </div>
         </section>
         <section>
           <div className="container max-w-[90%] relative">
-            {MenuTab.map((MenuTab, i) => (
-              <div key={i}>
-                {currentTab === `${MenuTab.id}` && (
-                  <div className="flex flex-col items-center text-center py-20">
-                    <p className="title">{MenuTab.title}</p>
-                    <p>{MenuTab.content}</p>
-                  </div>
-                )}
-              </div>
-            ))}
+            {chickenTab && <ChickenMenu />}
+            {porkTab && <PorkMenu />}
+            {beefTab && <BeefMenu />}
           </div>
         </section>
         <section>
