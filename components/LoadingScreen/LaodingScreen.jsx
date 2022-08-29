3282156956920ae6ba9c1e63329472c1a97da980
mjlcreative <markjaysonlacandula@gmail.com>
2022-08-30 02:53:55 +0800

    update

--- components/LoadingScreen/LaodingScreen.jsx
+++ components/LoadingScreen/LaodingScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { useIsVisible } from "react-is-visible";
 import FadeScreen from "../Animation/FadeScreen";
+import FadeUpTransition from "../Animation/FadeUpTransition";
 
 const LaodingScreen = () => {
   const nodeRef = useRef();
@@ -11,12 +12,15 @@ const LaodingScreen = () => {
       ref={nodeRef}
       className="flex justify-center items-center text-center bg-personal-primary w-screen h-screen"
     >
-      <p className="font-oswald font-bold text-5xl tablet:text-8xl text-white select-none">
-        MJL KITCHEN
-      </p>
+      <FadeUpTransition open={IsVisible}>
+        <p className="font-oswald font-bold text-5xl tablet:text-8xl text-white select-none">
+          MJL KITCHEN
+        </p>
+      </FadeUpTransition>
+
       <FadeScreen
         open={IsVisible}
-        className="flex top-0 absolute bg-personal-neutralDark z-20 w-screen h-screen"
+        className="flex top-0 absolute backdrop-blur-lg z-10 w-screen h-screen"
       ></FadeScreen>
     </div>
   );
